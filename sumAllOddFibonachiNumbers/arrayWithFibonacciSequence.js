e9f5707491468209bba7a70980b3b97c1ee76b63
vo0doo <kirsanov.bvt@gmail.com>
2022-08-27 15:43:42 +0300

    Sum All Odd Fibonacci Numbers complete

---
+++ sumAllOddFibonachiNumbers/arrayWithFibonacciSequence.js
@@ -0,0 +1,14 @@
+function arrayWithFibonacciSequence(num) {
+    const fibs = [1, 1];
+    let nextNumber;
+    while ((nextNumber = fibs[0]+fibs[1]) <= num) {
+        fibs.unshift(nextNumber)
+    }
+
+    return fibs.filter(i=>i%2).reduce((prev, curr)=>curr+prev, 0)
+}
+
+
+module.exports = {
+    arrayWithFibonacciSequence: arrayWithFibonacciSequence
+}
---
+++ sumAllOddFibonachiNumbers/whileLoop.js
@@ -0,0 +1,15 @@
+function whileLoop(num) {
+    let sum = 0; f1 = 1; f2 = 1;
+    while(f2 <= num) {
+        if (f2%2 !== 0) {
+            sum += f2
+        }
+        f1 += f2
+        f2 = f1 - f2
+    }
+    return sum
+}
+
+module.exports = {
+    whileLoop: whileLoop
+}
