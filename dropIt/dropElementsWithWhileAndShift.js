450e8d5ad0fa321c548f7c180d6731510ff7a02a
vo0doo <kirsanov.bvt@gmail.com>
2022-09-01 14:14:40 +0300

    update dropElementsWithWhileAndShift.js

--- dropIt/dropElementsWithWhileAndShift.js
+++ dropIt/dropElementsWithWhileAndShift.js
@@ -1,6 +1,11 @@
 function dropElementsWithWhileAndShift(arr, func) {
-    while (!func(arr[0])){
-        arr.shift()
+    while(!func(arr[0])) {
+        if (arr.length > 0){
+            arr.shift()
+        }
+        else {
+            return []
+        }
     }
     return arr
 }
