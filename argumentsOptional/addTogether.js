7edf781e48a79a70e22398c860f2dba4315e3443
vo0doo <kirsanov.bvt@gmail.com>
2022-09-02 13:29:43 +0300

    update addTogether.js - complete with me

--- argumentsOptional/addTogether.js
+++ argumentsOptional/addTogether.js
@@ -1,5 +1,14 @@
 function addTogether() {
-    return undefined
+    const [a, b] = [...arguments]
+    if (!Number.isInteger(a)) {
+        return undefined
+    }
+    if (arguments.length > 1) {
+        return Number.isInteger(a) && Number.isInteger(b) ? a + b : undefined
+    }
+    return function() {
+            return Number.isInteger(a) && Number.isInteger(arguments[0]) ? a + arguments[0] : undefined
+    }
 }
 
 
