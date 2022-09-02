784825812e196df90d0699b22ecb34a074ee1a1f
vo0doo <kirsanov.bvt@gmail.com>
2022-09-02 10:44:35 +0300

    update truthCheckWithEvery.js

--- everythinkBeTrue/truthCheckWithEvery.js
+++ everythinkBeTrue/truthCheckWithEvery.js
@@ -1,12 +1,5 @@
 function truthCheckWithEvery(collection, pre) {
-    
-    for(let i = 0; i < collection.length; i++) {
-        if (!Boolean(collection[i][pre])) {
-            return false
-        }
-    }
-
-    return true
+    return collection.every(elem=>Boolean(elem[pre]))
 }
 
 
