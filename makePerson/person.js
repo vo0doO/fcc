e616cc2695f949de46e69c75f4981c3e212612fb
vo0doo <kirsanov.bvt@gmail.com>
2022-09-03 10:50:52 +0300

    update person.js

--- makePerson/person.js
+++ makePerson/person.js
@@ -1,30 +1,26 @@
-const Person = function(firstAndLast) {
-  // Only change code below this line
-  // Complete the method below and implement the others similarly
-  let fullName = firstAndLast;
-  let firstName = fullName.split(" ")[0]
-  let lastName = fullName.split(" ")[1]
-  
-  this.getFirstName = function() {
-      return firstName
-  }
-  this.getLastName = function() {
-      return lastName
-  }
-  this.getFullName = function() {
-      return `${firstName} ${lastName}`
-  }
-  this.setFirstName = function(first) {
-      firstName = first
-  }
-  this.setLastName = function(last) {
-      lastName = last
-  }
-  this.setFullName = function(firstAndLast) {
-      firstName = firstAndLast.split(" ")[0]
-      lastName = firstAndLast.split(" ")[1]
-  }
-  return firstAndLast
+const Person = function (firstAndLast) {
+
+    let [firstName, lastName] = firstAndLast.split(" ")
+
+    this.getFirstName = function () {
+        return firstName
+    }
+    this.getLastName = function () {
+        return lastName
+    }
+    this.getFullName = function () {
+        return `${firstName} ${lastName}`
+    }
+    this.setFirstName = function (first) {
+        firstName = first
+    }
+    this.setLastName = function (last) {
+        lastName = last
+    }
+    this.setFullName = function (firstAndLast) {
+        [firstName, lastName] = firstAndLast.split(" ")
+    }
+    return firstAndLast
 };
 
 
