18cede29f9d46e52362c26ee4262037cdd862367
vo0doo <kirsanov.bvt@gmail.com>
2022-09-03 13:32:46 +0300

    update orbitalPeriod.js

--- mapTheDebris/orbitalPeriod.js
+++ mapTheDebris/orbitalPeriod.js
@@ -1,7 +1,15 @@
 function orbitalPeriod(arr) {
+
     const GM = 398600.4418;
     const earthRadius = 6367.4447;
-    return arr
+
+    function getOrbitalPeriod(planet) {
+        planet["orbitalPeriod"] = Math.round((Math.PI * 2) * Math.sqrt((Math.pow(planet["avgAlt"] + earthRadius, 3) / GM)))
+        const { name, orbitalPeriod } = planet
+        return { name, orbitalPeriod }
+    }
+    
+    return arr.map(getOrbitalPeriod)
 }
 
 
