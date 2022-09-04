1c05b16f815472b856b2ba9ad7e0ad4a8759a10a
vo0doo <kirsanov.bvt@gmail.com>
2022-09-04 16:29:21 +0300

    update rot13.js

--- caesarsCipher/rot13.js
+++ caesarsCipher/rot13.js
@@ -1,7 +1,7 @@
 function rot13(str) {
     return String.fromCharCode(...str.split("").map(
         (i) => {
-            if (i.charCodeAt(0) < 65) {
+            if (65 > i.charCodeAt(0) || i.charCodeAt(0) > 122) {
                 return i.charCodeAt()
             }
             else if ((i.charCodeAt(0) - 13) < 65) {
