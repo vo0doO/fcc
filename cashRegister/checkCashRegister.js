2bd99b5cc2227e8e1b681f95cf79e51b7b2d86ed
vo0doo <kirsanov.bvt@gmail.com>
2022-09-05 16:02:10 +0300

    update checkCashRegister.js

--- cashRegister/checkCashRegister.js
+++ cashRegister/checkCashRegister.js
@@ -1,6 +1,6 @@
 function checkCashRegister(price, cash, cid) {
 
-    const amountArray = [["ONE HUNDRED", 100], ["TWENTY", 20], ["TEN", 10], ["FIVE", 5], ["ONE", 1], ["QUARTER", 0.25], ["DIME", 0.1], ["NICKEL", 0.05], ["PENNY", 0.01]]
+    const nominal = [["ONE HUNDRED", 100], ["TWENTY", 20], ["TEN", 10], ["FIVE", 5], ["ONE", 1], ["QUARTER", 0.25], ["DIME", 0.1], ["NICKEL", 0.05], ["PENNY", 0.01]]
     const change = [["ONE HUNDRED", 0], ["TWENTY", 0], ["TEN", 0], ["FIVE", 0], ["ONE", 0], ["QUARTER", 0], ["DIME", 0], ["NICKEL", 0], ["PENNY", 0]]
     cid = cid.reverse()
 
@@ -9,15 +9,27 @@ function checkCashRegister(price, cash, cid) {
     let amountChange = cash - price
 
     if (amountSum == amountChange) {
-        return { status: "CLOSED", change: cid }
+        return { status: "CLOSED", change: cid.reverse() }
     }
 
-    else {
-        return {
-            "status": "OPEN",
-            "change": []
+    for (let i = 0; i < cid.length; i++) {
+        if (amountChange == 0) {
+            return { status: "OPEN", change: change.filter(i => i[1] > 0).reverse() }
+        }
+        while (parseFloat(amountChange).toFixed(2) >= nominal[i][1] && cid[i][1] != 0) {
+            change[i][1] += nominal[i][1]
+            amountChange -= nominal[i][1]
+            cid[i][1] -= nominal[i][1]
         }
     }
+
+    let changeSum = change.map(i => i[1]).reduce((a, b) => a + b, 0)
+
+    if (changeSum < amountChange) {
+        return { status: "INSUFFICIENT_FUNDS", change: [] }
+    }
+
+    return { status: "OPEN", change: change.filter(i => i[1] > 0) }
 
 }
 
