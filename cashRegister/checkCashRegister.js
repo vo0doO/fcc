a5d605f07a86ed776b9e299307bdbbc1c19f9f66
vo0doo <kirsanov.bvt@gmail.com>
2022-09-05 08:22:15 +0300

    update checkCashRegister.js

--- cashRegister/checkCashRegister.js
+++ cashRegister/checkCashRegister.js
@@ -1,6 +1,8 @@
 function checkCashRegister(price, cash, cid) {
 
     const amountArray = [["ONE HUNDRED", 100], ["TWENTY", 20], ["TEN", 10], ["FIVE", 5], ["ONE", 1], ["QUARTER", 0.25], ["DIME", 0.1], ["NICKEL", 0.05], ["PENNY", 0.01]]
+    const change = [["ONE HUNDRED", 0], ["TWENTY", 0], ["TEN", 0], ["FIVE", 0], ["ONE", 0], ["QUARTER", 0], ["DIME", 0], ["NICKEL", 0], ["PENNY", 0]]
+    cid = cid.reverse()
 
     let amountSum = cid.map(i => i[1]).reduce((a, b) => a + b, 0)
 
