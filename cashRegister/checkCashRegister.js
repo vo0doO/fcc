8316342c14f651778a03bfb50ca6f857181669fc
vo0doo <kirsanov.bvt@gmail.com>
2022-09-05 08:13:17 +0300

    update checkCashRegister.js

--- cashRegister/checkCashRegister.js
+++ cashRegister/checkCashRegister.js
@@ -1,8 +1,22 @@
 function checkCashRegister(price, cash, cid) {
-    return {
-        "status": "INSUFFICIENT_FUNDS",
-        "change": []
+
+    const amountArray = [["ONE HUNDRED", 100], ["TWENTY", 20], ["TEN", 10], ["FIVE", 5], ["ONE", 1], ["QUARTER", 0.25], ["DIME", 0.1], ["NICKEL", 0.05], ["PENNY", 0.01]]
+
+    let amountSum = cid.map(i => i[1]).reduce((a, b) => a + b, 0)
+
+    let amountChange = cash - price
+
+    if (amountSum == amountChange) {
+        return { status: "CLOSED", change: cid }
     }
+
+    else {
+        return {
+            "status": "OPEN",
+            "change": []
+        }
+    }
+
 }
 
 
