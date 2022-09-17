632b2ecb375258c919772a19f40bf91f4d10f52f
vo0doo <kirsanov.bvt@gmail.com>
2022-09-17 17:21:31 +0300

    update sym.js

--- codingInterviewPrep/algorithms/findTheSymmetricDifference/sym.js
+++ codingInterviewPrep/algorithms/findTheSymmetricDifference/sym.js
@@ -1,5 +1,20 @@
-function sym(args) {
-    return [3, 3, 3]
+function sym(...args) {
+    const sortFunc = (a, b) => a - b
+    return [
+        ...new Set(
+            args.reduce(
+                (prev, curr, idx) => {
+                    return prev.filter(
+                        i => !curr.includes(i)
+                    ).concat(
+                        curr.filter(
+                            i => !prev.includes(i)
+                        )
+                    )
+                }
+            )
+        )
+    ].sort(sortFunc)
 }
 
 module.exports = {
