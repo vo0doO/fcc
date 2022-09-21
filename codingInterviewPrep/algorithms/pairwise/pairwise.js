23ceeb31637246c7512b524d8ab07801ed9b4b05
vo0doo <kirsanov.bvt@gmail.com>
2022-09-21 12:25:39 +0300

    update pairwise.js

--- codingInterviewPrep/algorithms/pairwise/pairwise.js
+++ codingInterviewPrep/algorithms/pairwise/pairwise.js
@@ -1,3 +1,34 @@
 function pairwise(arr, numb) {
-    return numb
+    let arrCopy = arr.slice()// создаем копию массива для тестирования
+    let result = 0 // создаем переменную для записи результатов(индексов) - двумерный массив
+    nextValue:
+    for (let i = 0; i < arrCopy.length; i++) // перебираем тестовый массив с удалениему 
+    {
+        nextNextValue:
+        for (let j = 0; j < arrCopy.length; j++) // перебираем тестовый массив 
+        {
+            if (arrCopy[i] != -1 && arrCopy[j] != -1) {
+                let sum = arrCopy[i] + arrCopy[j]
+                if (sum == numb) {
+                    console.log(`Зашло: ${arrCopy[i]}[${i}] + ${arrCopy[j]}[${j}] = ${sum}`)
+                    result += i
+                    result += j
+                    arrCopy[i] = -1
+                    arrCopy[j] = -1
+                    continue nextValue
+                }
+            }
+        }
+
+        console.log('========')
+    }
+    return result
+}
+
+
+pairwise([1, 4, 2, 3, 0, 5], 7)
+
+
+module.exports = {
+    pairwise
 }
