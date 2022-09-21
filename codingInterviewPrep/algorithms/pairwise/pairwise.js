7a8959c976bd7978c1af7eb3537ff6b89da00460
vo0doo <kirsanov.bvt@gmail.com>
2022-09-21 15:44:30 +0300

    update pairwise.js - complete with named for loop with comments

--- codingInterviewPrep/algorithms/pairwise/pairwise.js
+++ codingInterviewPrep/algorithms/pairwise/pairwise.js
@@ -1,32 +1,23 @@
 function pairwise(arr, numb) {
-    let arrCopy = arr.slice()// создаем копию массива для тестирования
-    let result = 0 // создаем переменную для записи результатов(индексов) - двумерный массив
-    nextValue:
-    for (let i = 0; i < arrCopy.length; i++) // перебираем тестовый массив с удалениему 
-    {
-        nextNextValue:
-        for (let j = 0; j < arrCopy.length; j++) // перебираем тестовый массив 
-        {
-            if (arrCopy[i] != -1 && arrCopy[j] != -1) {
-                let sum = arrCopy[i] + arrCopy[j]
-                if (sum == numb) {
-                    console.log(`Зашло: ${arrCopy[i]}[${i}] + ${arrCopy[j]}[${j}] = ${sum}`)
-                    result += i
-                    result += j
-                    arrCopy[i] = -1
-                    arrCopy[j] = -1
-                    continue nextValue
+    let result = 0 // инициализируем переменную для записи результата
+    nextValue: // именуем цикл
+    for (let i = 0; i < arr.length; i++) { // перебираем массив со счетчиком i 
+        for (let j = 0; j < arr.length; j++) { // перебираем массив со счетчиком j
+            if (arr[i] != -1 && arr[j] != -1 && i != j) { // если i-e и j-e значения в массиве не -1 и i и j не равны 
+                if (arr[i] + arr[j] === numb) { // если сумма i-го и j-го значений равны numb
+                    result += i + j // добавляем в результат сумму индексов i-го и j-го значений
+                    arr[i] = -1 // присвиваем -1 значению в массиве по индеку i 
+                    arr[j] = -1 // присвиваем -1 значению в массиве по индеку j
+                    continue nextValue // переходим к следующей итерации цикла со счётчиком i
                 }
             }
         }
-
-        console.log('========')
     }
     return result
 }
 
 
-pairwise([1, 4, 2, 3, 0, 5], 7)
+// pairwise([1, 4, 2, 3, 0, 5], 7)
 
 
 module.exports = {
