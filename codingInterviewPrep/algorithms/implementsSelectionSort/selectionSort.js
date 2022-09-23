6f110388a39bd41a7f34ce3daa476e5cd4d58a64
vo0doo <kirsanov.bvt@gmail.com>
2022-09-23 16:26:18 +0300

    update selectionSort.js

--- codingInterviewPrep/algorithms/implementsSelectionSort/selectionSort.js
+++ codingInterviewPrep/algorithms/implementsSelectionSort/selectionSort.js
@@ -1,7 +1,13 @@
 function selectionSort(array) {
-    return array.sort((a, b) => a - b)
+    for (let i in array) {  // для i=0, i+1, i+2... не более длины array
+        let min = Math.min(...array.slice(i))   // находим минимальное значение в несортированной части array
+        let minI = array.indexOf(min, i)    // находим в несортированной части array индекс минимального значения
+        let swap = array[i] // записываем i-e значение в переменную
+        array[i] = min  // i-e значение заменяем минимальным
+        array[minI] = swap  // минимальное значение заменяем i-ым
+    }
+    return array
 }
-
 
 module.exports = {
     selectionSort
