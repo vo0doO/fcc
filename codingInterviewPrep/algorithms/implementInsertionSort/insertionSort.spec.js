e8c9b4bfd79ff2ec092fe48c6b470df9a8d196ad
vo0doo <kirsanov.bvt@gmail.com>
2022-10-01 10:50:04 +0300

    test: update insertionSort.spec.js

--- codingInterviewPrep/algorithms/implementInsertionSort/insertionSort.spec.js
+++ codingInterviewPrep/algorithms/implementInsertionSort/insertionSort.spec.js
@@ -1,6 +1,10 @@
+const fs = require("fs")
 const assert = require("assert")
 const { insertionSort } = require('./insertionSort')
+
+
 const sortFunc = (a, b) => a - b
+const filePath = "codingInterviewPrep/algorithms/implementInsertionSort/insertionSort.js"
 
 describe('Implements Selection Sort', () => {
     it('insertionSort должна быть функция', () => {
@@ -8,5 +12,8 @@ describe('Implements Selection Sort', () => {
     })
     it('insertionSort([1,4,2,8,345,123,43,32,5643,63,123,43,2,55,1,234,92]) должен возвращать массив, который не изменился, за исключением порядка', () => {
         assert.deepEqual([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92].sort(sortFunc), insertionSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]))
+    })
+    it('insertionSort не должен использовать стандартный метод .sort', () => {
+        assert.deepEqual(false, fs.readFileSync(filePath).asciiSlice().includes(".sort("))
     })
 })
