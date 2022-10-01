9591163d9160e4f235f5e9acac473ad4b4640476
vo0doo <kirsanov.bvt@gmail.com>
2022-10-01 11:19:40 +0300

    test: update bubbleSort.spec.js

--- codingInterviewPrep/algorithms/implementBubbleSort/bubbleSort.spec.js
+++ codingInterviewPrep/algorithms/implementBubbleSort/bubbleSort.spec.js
@@ -1,6 +1,10 @@
+const fs = require("fs")
 const assert = require('assert');
 const { bubbleSort } = require("./bubbleSort");
+
+
 const sortFunc = (a, b) => a - b
+const filePath = "codingInterviewPrep/algorithms/implementBubbleSort/bubbleSort.js"
 
 describe('Inmplement Bubble Sort', () => {
     it('bubbleSort должно быть function.', () => {
@@ -10,5 +14,8 @@ describe('Inmplement Bubble Sort', () => {
         assert.deepEqual(
             [1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92].sort(sortFunc),
             bubbleSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92]))
+    })
+    it('bubbleSort не должен использовать стандартный метод .sort', () => {
+        assert.deepEqual(false, fs.readFileSync(filePath).asciiSlice().includes(".sort("))
     })
 })
