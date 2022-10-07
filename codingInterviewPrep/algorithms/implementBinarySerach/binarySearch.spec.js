280410dd2acf1782a368ea213cc6d256aaea5902
vo0doo <kirsanov.bvt@gmail.com>
2022-10-07 19:27:10 +0300

    test: create binarySearch.spec.js

---
+++ codingInterviewPrep/algorithms/implementBinarySerach/binarySearch.spec.js
@@ -0,0 +1,45 @@
+const assert = require('assert')
+const binarySearch = require('./binarySearch')
+
+
+const testArray = [
+    0, 1, 2, 3, 4, 5, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+    23, 49, 70
+];
+
+
+describe("Implement Binary Search", () => {
+    it('binarySearch должно быть функцией.', () => {
+        assert.equal('function', typeof binarySearch)
+    })
+    it('binarySearch(testArray, 0) должно вернуться[13, 5, 2, 0]', () => {
+        assert.deepEqual([13, 5, 2, 0], binarySearch(testArray, 0))
+    })
+    it('binarySearch(testArray, 1) должно вернуться[13, 5, 2, 0, 1]', () => {
+        assert.deepEqual([13, 5, 2, 0, 1], binarySearch(testArray, 1))
+    })
+    it('binarySearch(testArray, 2) должно вернуться[13, 5, 2]', () => {
+        assert.deepEqual([13, 5, 2], binarySearch(testArray, 2))
+    })
+    it('binarySearch(testArray, 6) должно вернуть строку Value Not Found', () => {
+        assert.equal('Value Not Found', binarySearch(testArray, 6))
+    })
+    it('binarySearch(testArray, 11) должно вернуться[13, 5, 10, 11]', () => {
+        assert.deepEqual([13, 5, 10, 11], binarySearch(testArray, 11))
+    })
+    it('binarySearch(testArray, 13) должно вернуться[13]', () => {
+        assert.deepEqual([13], binarySearch(testArray, 13))
+    })
+    it('binarySearch(testArray, 70) должно вернуться[13, 19, 22, 49, 70]', () => {
+        assert.deepEqual([13, 19, 22, 49, 70], binarySearch(testArray, 70))
+    })
+
+})
+
+
+
+
+
+
+
+
