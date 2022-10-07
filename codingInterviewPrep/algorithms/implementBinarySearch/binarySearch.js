b7dfcdd58eb733df91a32277a124fbd9350c7604
vo0doo <kirsanov.bvt@gmail.com>
2022-10-07 22:25:56 +0300

    update binarySearch.js and binarySearch.spec.js

--- codingInterviewPrep/algorithms/implementBinarySearch/binarySearch.js
+++ codingInterviewPrep/algorithms/implementBinarySearch/binarySearch.js
@@ -1,19 +1,28 @@
-function binarySearch(array, number) {
-    let result = [] // инициализация результирующего массива
+function binarySearch(array, number, result = []) {
     // первый базовый случай
-    if (array.length == 0) { // если длина массива 0
-        return result // возвращаем результат
+    if (array.length <= 1) { // если длина массива 0
+        console.log(array[0], number)
+        if (array[0] == number) {
+            result.push(array[0])
+            return result
+        }
+        return "Value Not Found" // возвращаем результат
     }
-    // второй базовый случай
     let middleIndex = Math.floor(array.length / 2) // находим индекс центрального элемента
-    let middleValue = array[middleIndex] // записываем центральное значение в переменную
+    if (middleIndex % 2) { }
+    let middleValue = array[middleIndex - 1] // записываем центральное значение в переменную
+    // второй базовый случай
     if (middleValue == number) { // если центральное значение равно искомому значению
-        result.push(number)   // добавляем искомое значени в результат
+        result.push(number)   // добавляем искомое значение в результат
         return result   // возвращаем результат
     }
     // рекурсивный случай
     result.push(middleValue) // добавляем центральное значение в результат
-    return (middleValue > number) ? binarySearch(array.slice(0, middleIndex), number) : binarySearch(array.slice(middleIndex), number)  // если центральное значение больше искомого то повторяем все с левой частью массива если меньше то с правой
+    let left = array.slice(0, middleIndex)
+    let right = array.slice(middleIndex)
+    console.log("Left: ", left)
+    console.log("Right: ", right)
+    return (middleValue > number) ? binarySearch(left, number, result) : binarySearch(right, number, result)  // если центральное значение больше искомого то повторяем все с левой частью массива если меньше то с правой
 }
 
 module.exports = {
