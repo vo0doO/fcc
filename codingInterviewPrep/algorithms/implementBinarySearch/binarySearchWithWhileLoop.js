44f13cf44f003f5845847778f9260a48f320fb70
vo0doo <kirsanov.bvt@gmail.com>
2022-10-17 12:09:03 +0300

    update binarySearchWithWhileLoop.js

--- codingInterviewPrep/algorithms/implementBinarySearch/binarySearchWithWhileLoop.js
+++ codingInterviewPrep/algorithms/implementBinarySearch/binarySearchWithWhileLoop.js
@@ -2,14 +2,14 @@ function binarySearchWithWhileLoop(arr, key) {
     let low = 0
     let high = arr.length - 1
     let result = []
-    while (low <= high) {                         
-        let mid = Math.floor((low + (high-low)/2))                                          
+    while (low <= high) {
+        let mid = Math.floor((low + (high - low) / 2))
         let midVal = arr[mid]
         if (midVal < key) {
             result.push(midVal)
             low = mid + 1
         }
-        else if (midVal > key) {                      
+        else if (midVal > key) {
             result.push(midVal)
             high = mid - 1
         }
