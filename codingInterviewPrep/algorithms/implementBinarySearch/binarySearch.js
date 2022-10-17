34df053bf3b726b8d875479ae6bd6added158a88
vo0doo <kirsanov.bvt@gmail.com>
2022-10-17 12:01:08 +0300

    update binarySearch.js

--- codingInterviewPrep/algorithms/implementBinarySearch/binarySearch.js
+++ codingInterviewPrep/algorithms/implementBinarySearch/binarySearch.js
@@ -1,27 +1,41 @@
-function binarySearch(array, number, result = []) {
-    // первый базовый случай
-    if (array.length <= 1) { // если длина массива 0
-        console.log(array[0], number)
-        if (array[0] == number) {
-            result.push(array[0])
-            return result
-        }
-        return "Value Not Found" // возвращаем результат
+function binarySearch(array, number, result, low, high) {
+    /**
+     * Находим индекс центрального элемента
+     * записываем значение по этому индексу в переменную
+     */
+    let mid = Math.floor((low + (high - low)) / 2)
+    let midVal = array[mid]
+    /**
+     * Первый базовый случай:
+     * Если длина массива равна 0
+     * искомое значение не найдено
+     */
+    if (array.length == 0) {
+        return "Value Not Found"
     }
-    let middleIndex = Math.floor(array.length / 2) // находим индекс центрального элемента
-    let middleValue = array[middleIndex - 1] // записываем центральное значение в переменную
-    // второй базовый случай
-    if (middleValue == number) { // если центральное значение равно искомому значению
-        result.push(number)   // добавляем искомое значение в результат
-        return result   // возвращаем результат
+    /**
+     * Второй базовый случай:
+     * Если центральное значение равно искомому
+     * добавляем его в результат и возвращаем результат
+     */
+    if (midVal == number) {
+        result.push(number)  
+        return result  
     }
-    // рекурсивный случай
-    result.push(middleValue) // добавляем центральное значение в результат
-    let left = array.slice(0, middleIndex)
-    let right = array.slice(middleIndex)
-    console.log("Left: ", left)
-    console.log("Right: ", right)
-    return (middleValue > number) ? binarySearch(left, number, result) : binarySearch(right, number, result)  // если центральное значение больше искомого то повторяем все с левой частью массива если меньше то с правой
+
+    /** 
+     * Рекурсивный случай:
+     * Добавляем центральное значение в результат
+     * и опеределяем левую и правую части массива
+     * Если центральное значение больше искомого
+     * то повторяем все с левой частью, иначе с правой
+    */
+    result.push(midVal) 
+    let left = array.slice(0, mid)
+    let right = array.slice(mid+1)
+    return (midVal > number) ? 
+        binarySearch(left, number, result, 0, left.length - 1) :
+        binarySearch(right, number, result, 0, right.length - 1)
 }
 
 module.exports = {
